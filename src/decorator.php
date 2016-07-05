5c39bc44bc374db6141bd9c16ab77be4989542e2
techworker <benjaminansbach@googlemail.com>
2016-07-05 23:30:59 +0200

    Docs and git changes

--- src/decorator.php
+++ src/decorator.php
@@ -2,31 +2,32 @@
 
 namespace techworker\fn\sortby;
 
-/**
- * A decorator function to transform a comparator of any type to a comparator
- * usable in combination with usort.
+/*
+ * A decorator function to transform a comparator of any type to a comparator usable in
+ * combination with usort/uasort.
  *
- * @param mixed $comparator The comparator value.
- * @param int $direction
+ * @param int|string|callable $comparator The comparator value.
+ * @param int $direction The sort direction.
  */
-return (/* PHPStorm */$a = function /*decorator*/ ($comparator, $direction = \SORT_ASC) : callable {
+return /* PHPStorm */$a = function ($comparator, $direction = \SORT_ASC) : callable {
     // check if the given value is not a function
     if (!is_callable($comparator)) {
         // make unary function
         $comparator = function ($v) use ($comparator) {
-            if(is_array($v) && isset($v[$comparator])) {
+            if (is_array($v) && isset($v[$comparator])) {
                 return $v[$comparator];
             }
-            if(is_object($v) && isset($v->{$comparator})) {
+            if (is_object($v) && isset($v->{$comparator})) {
                 return $v->{$comparator};
             }
 
-            assert('false', 'The property or key you gave to techworker\fn\\sortby does not exist.');
-            return "";
+            assert('false', 'The property or key you gave to techworker\fn\\sortBy|thenBy does not exist.');
+            return 0;
         };
     }
 
-    // now check how many parameters the function wants to have
+    //  now check how many parameters the function has, if only 1 we will wrap it into a real
+    // comparer function
     if ((new \ReflectionFunction($comparator))->getNumberOfParameters() === 1) {
         $comparator = function ($v1, $v2) use ($comparator) {
             return $comparator($v1) <=> $comparator($v2);
@@ -34,7 +35,7 @@ return (/* PHPStorm */$a = function /*decorator*/ ($comparator, $direction = \SO
     }
 
     // check if the sort mode is descending
-    if ($direction === SORT_DESC) {
+    if ($direction === \SORT_DESC) {
         // make another decorator and return the inverted cmp value
         return function ($v1, $v2) use ($comparator) {
             return -$comparator($v1, $v2);
@@ -42,4 +43,4 @@ return (/* PHPStorm */$a = function /*decorator*/ ($comparator, $direction = \SO
     }
 
     return $comparator;
-});
+};
